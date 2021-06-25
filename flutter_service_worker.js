472c0a00879ec280007666f77ae996f59a8c6d55
CarlosMedina10 <62670609+CarlosMedina10@users.noreply.github.com>
2021-06-25 04:41:56 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {
 "assets/assets/images/sorteoForocoches.jpg": "e3f72a39e966693b62bec117935f11f0",
 "assets/FontManifest.json": "b4bf71d5c0024db6df34ba0934c54e07",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "438e7e58673b35ef6e99865b49892fd9",
+"assets/NOTICES": "f571a282877c66acbaf6068445ae38d4",
 "assets/packages/community_material_icon/fonts/materialdesignicons-webfont.ttf": "b0fd91bb29dcb296a9a37f8bda0a2d85",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/flutter_auth_buttons/fonts/Roboto-Medium.ttf": "d08840599e05db7345652d3d417574a9",
@@ -49,9 +49,9 @@ const RESOURCES = {
 "favicon.ico": "37b27401b90127a4490716a9a7bdf96c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "3c318fff2233d809382f0cafbc9a51e7",
-"/": "3c318fff2233d809382f0cafbc9a51e7",
-"main.dart.js": "ee393e30ed8733be8d9845f308d32afe",
+"index.html": "db525a77250188569d34ad6333a9d48b",
+"/": "db525a77250188569d34ad6333a9d48b",
+"main.dart.js": "f8fa0ec9f289e972f4741c0af019f270",
 "manifest.json": "ebcf31622a95108114c7032090ad2ffb",
 "version.json": "37fc8616f8aac3766d996a983fa11da6"
 };
