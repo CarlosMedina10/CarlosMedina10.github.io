d570de65f3ff7186692a6a3028c2d441880d32ed
CarlosMedina10 <62670609+CarlosMedina10@users.noreply.github.com>
2021-06-25 18:36:12 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -49,9 +49,9 @@ const RESOURCES = {
 "favicon.ico": "37b27401b90127a4490716a9a7bdf96c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "ab0fc27d185c53967fc09c6e5781a358",
-"/": "ab0fc27d185c53967fc09c6e5781a358",
-"main.dart.js": "4cfe432f93c59654b4e98fad7d483528",
+"index.html": "21a1c827ba03448ab40e5583511ec94a",
+"/": "21a1c827ba03448ab40e5583511ec94a",
+"main.dart.js": "051632233d5da9663c2a51e98d07f130",
 "manifest.json": "ebcf31622a95108114c7032090ad2ffb",
 "version.json": "37fc8616f8aac3766d996a983fa11da6"
 };
